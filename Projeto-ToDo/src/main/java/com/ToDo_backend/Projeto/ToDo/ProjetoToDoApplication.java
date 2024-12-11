5b41e387c81c23c8ef486dd2bb733e37084cc92d
TauanAnjos <tauandosanjosnascimento@gmail.com>
2024-12-11 11:04:54 -0300

    inicializando projeto ToDo

---
+++ Projeto-ToDo/src/main/java/com/ToDo_backend/Projeto/ToDo/ProjetoToDoApplication.java
@@ -0,0 +1,13 @@
+package com.ToDo_backend.Projeto.ToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoToDoApplication.class, args);
+	}
+
+}
---
+++ Projeto-ToDo/src/test/java/com/ToDo_backend/Projeto/ToDo/ProjetoToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ToDo_backend.Projeto.ToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
