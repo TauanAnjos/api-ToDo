38892ef97070c82d94e87eb9e287efbac2c60b73
TauanAnjos <tauandosanjosnascimento@gmail.com>
2024-12-13 11:17:28 -0300

    Implementado configuração para conexão com frontend.

--- Projeto-ToDo/src/main/java/com/ToDo_backend/Projeto/ToDo/config/SecurityConfig.java
+++ Projeto-ToDo/src/main/java/com/ToDo_backend/Projeto/ToDo/config/SecurityConfig.java
@@ -3,20 +3,35 @@ package com.ToDo_backend.Projeto.ToDo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/user").permitAll()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user").permitAll()
                         .requestMatchers("/h2/**").permitAll()
                         .anyRequest().permitAll() // Permitir acesso a todas as solicitações
                 )
                 .csrf(csrf -> csrf.disable()) // Desativar CSRF
-                .headers(headers -> headers.frameOptions().sameOrigin());
+                .headers(headers -> headers.frameOptions().sameOrigin()).cors();
 
         return http.build();
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
 }
