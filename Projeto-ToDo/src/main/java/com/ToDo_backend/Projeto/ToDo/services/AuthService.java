0df42af8ccdfca1d6520e078664dff187bfc6ab4
tauandosanjosnascimento <tauandosanjosnascimento@gmail.com>
2025-01-15 12:44:10 -0300

    Tratando exceção au se autenticar.

--- Projeto-ToDo/src/main/java/com/ToDo_backend/Projeto/ToDo/services/AuthService.java
+++ Projeto-ToDo/src/main/java/com/ToDo_backend/Projeto/ToDo/services/AuthService.java
@@ -4,8 +4,10 @@ import com.ToDo_backend.Projeto.ToDo.exception.BusinessRuleException;
 import com.ToDo_backend.Projeto.ToDo.models.UserModel;
 import com.ToDo_backend.Projeto.ToDo.repositories.UserRepository;
 import com.ToDo_backend.Projeto.ToDo.rest.dtos.UserDTORequest;
+import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,13 +23,18 @@ public class AuthService {
 
     public String authentication(String email, String password){
 
-        UsernamePasswordAuthenticationToken usernamePassword =
-                new UsernamePasswordAuthenticationToken(email, password);
+       try{
+           UsernamePasswordAuthenticationToken usernamePassword =
+                   new UsernamePasswordAuthenticationToken(email, password);
+           authenticationManager.authenticate(usernamePassword);
 
-        authenticationManager.authenticate(usernamePassword);
+           return tokenService.generateToken(email);
 
-        String token = tokenService.generateToken(email);
+       }catch (BadCredentialsException e){
+           throw new BusinessRuleException("E-mail ou senha inválida." , e);
+       }catch (Exception e){
+           throw new BusinessRuleException("Erro ao tentar autenticar usuário.", e);
+       }
 
-        return token;
     }
 }
